057319fed9a0bc6e8261246b843c2fce2ebd13fa
Fiza Anish <fizaanish24@gmail.com>
2026-06-12 13:31:10 +0530

    Added temperature sensing and pwm motor

---
+++ TEMP_PROJECT/pwm.c
@@ -0,0 +1,35 @@
+/*
+ * pwm.c
+ *
+ * Created: 09-06-2026 15:14:26
+ *  Author: ACER
+  definition doesnt need ;*/ 
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+
+void pwm0a_config()
+{
+	/*creating output pin for pwm on d6*/
+
+	DDRD |= (1<<DDD6);
+
+	//FAST PWM
+	TCCR0A |= (1<<WGM00);
+	TCCR0A |= (1<<WGM01);
+	// non INVERTING PWM
+	TCCR0A &= ~(1<<COM0A0);
+	TCCR0A |= (1<<COM0A1);
+	//PRESCALAR TIMER CLK/1024
+	TCCR0B &=~(1<<CS02);
+	TCCR0B |=(1<<CS01);
+	TCCR0B |=(1<<CS00);
+
+	OCR0A = 154;
+}
+
+void pwm_duty(int duty){
+	
+	int A = (duty*256) /100;
+	OCR0A = A ;
+}
---
+++ TEMP_PROJECT/pwm.h
@@ -0,0 +1,17 @@
+/*
+ * pwm.h
+ *
+ * Created: 09-06-2026 15:13:57
+ *  Author: ACER
+ * function decleration*/ 
+
+
+#ifndef PWM_H_
+#define PWM_H_
+
+void pwm0a_config();
+void pwm_duty(int duty);
+
+
+
+#endif /* PWM_H_ */
